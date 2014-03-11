53b0bd2e17d2f23bea498d90de1900877e321bfd
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-11 14:30:09 +0100

    move scripts to bottom of body

--- js/application.js
+++ js/application.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Wait till the browser is ready to render the game (avoids glitches)
-  window.requestAnimationFrame(function () {
-    var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
-  });
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
 });

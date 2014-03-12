abf45925f6f75ab64e3508eec60827c162313062
petitssoldats <forestier.mk@gmail.com>
2014-03-12 16:12:39 +0100

    add fastclick on dom for touch device

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
+  FastClick.attach(document.body);
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });

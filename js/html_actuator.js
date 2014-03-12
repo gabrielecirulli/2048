119d8ecdd0182290fa698f726a38092969ba23f4
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-12 13:05:55 +0100

    restore ga in gh-pages

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -117,6 +117,10 @@ HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
 
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "end", type, this.score);
+  }
+
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
 

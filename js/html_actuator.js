af61d052cbe49951200118c16117e3f6c04b3e6b
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-14 23:09:25 +0100

    remove twitter uri

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -156,7 +156,6 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
   tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
   tweet.textContent = "Tweet";
 
   var text = "I scored " + this.score + " points at 2048, a game where you " +

10c72a76b3c8e2aa496cdf1400b305d1ae960dba
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-14 13:35:42 +0100

    rename tile-gold to tile-super

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 2048) classes.push("tile-gold");
+  if (tile.value > 2048) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 

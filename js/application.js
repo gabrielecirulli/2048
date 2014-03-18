d91afb6bb7cb007e0b7e132cdce10a015253e059
Daniel Kleinstein <Dkl1456@gmail.com>
2014-03-18 22:26:42 +0200

    Updated colors issue, added small images

--- js/application.js
+++ js/application.js
@@ -2,3 +2,31 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
+var imageList = [
+       "tzvika/212/tzvika2.png",
+       "tzvika/212/tzvika4.png",
+       "tzvika/212/tzvika8.png",
+       "tzvika/212/tzvika16.png",
+       "tzvika/212/tzvika32.png",
+       "tzvika/212/tzvika64.png",
+       "tzvika/212/tzvika128.png",
+       "tzvika/212/tzvika256.png",
+       "tzvika/212/tzvika512.png",
+       "tzvika/212/tzvika1024.png",
+       "tzvika/212/tzvika2048.png",
+       "tzvika/114/tzvika2small.png",
+       "tzvika/114/tzvika4small.png",
+       "tzvika/114/tzvika8small.png",
+       "tzvika/114/tzvika16small.png",
+       "tzvika/114/tzvika32small.png",
+       "tzvika/114/tzvika64small.png",
+       "tzvika/114/tzvika128small.png",
+       "tzvika/114/tzvika256small.png",
+       "tzvika/114/tzvika512small.png",
+       "tzvika/114/tzvika1024small.png",
+       "tzvika/114/tzvika2048small.png",
+];
+for (var i = 0; i < imageList.length; i++) {
+    var imageObject = new Image();
+    imageObject.src = imageList[i];
+}

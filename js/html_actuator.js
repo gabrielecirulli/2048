d266c81718fbcdff4e36146913ce45ff9c123982
Lou Huang <lou@codeforamerica.org>
2014-03-18 20:37:46 -0700

    even less numberwang

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,15 +62,14 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  var wangValue = Math.round(Math.random() * tile.value);
-
   if (tile.value > 2048) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
 //  inner.textContent = tile.value;
-  inner.textContent = wangValue;
+  tile.generateWangValue();
+  inner.textContent = tile.wangValue;
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
--- js/tile.js
+++ js/tile.js
@@ -2,9 +2,34 @@ function Tile(position, value) {
   this.x                = position.x;
   this.y                = position.y;
   this.value            = value || 2;
+  this.wangValue        = null;
 
   this.previousPosition = null;
   this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.generateWangValue = function () {
+  var value = this.value,
+      wang  = '',
+      random = Math.random(),
+      chars = 'abcdefghijklmnopqrstuvwxyz';
+
+  wang = Math.ceil(Math.random() * (value - (value/2)) * 4);
+
+  if (random > 0.94) {
+    wang = wang.toString() + '.' + Math.floor(Math.random() * 10).toString();
+  }
+  else if (random < 0.04) {
+    wang = '-' + wang.toString();
+  }
+  else if (random > 0.04 && random < 0.05) {
+    wang = chars[Math.floor(Math.random() * chars.length)];
+  }
+  else if (random > 0.05 && random < 0.08) {
+    wang = wang + Math.floor(Math.random() * 10000);
+  }
+
+  this.wangValue = wang;
 }
 
 Tile.prototype.savePosition = function () {

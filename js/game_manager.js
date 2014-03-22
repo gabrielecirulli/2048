8ad8318c7ce84298b17dcf0aa15f357926cb2ffa
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-22 17:06:37 +0100

    align assignments

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,10 +1,10 @@
 function GameManager(size, InputManager, Actuator, StorageManager) {
-  this.size         = size; // Size of the grid
-  this.inputManager = new InputManager;
+  this.size           = size; // Size of the grid
+  this.inputManager   = new InputManager;
   this.storageManager = new StorageManager;
-  this.actuator     = new Actuator;
+  this.actuator       = new Actuator;
 
-  this.startTiles   = 2;
+  this.startTiles     = 2;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));

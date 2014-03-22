08c50df9386e4003a76a5fb03a3941516531403f
Gabriele Cirulli <gabrycir@gmail.com>
2014-03-22 15:38:52 +0100

    rename LocalScoreManager to LocalStorageManager

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -18,7 +18,7 @@ window.fakeStorage = {
   }
 };
 
-function LocalScoreManager() {
+function LocalStorageManager() {
   this.bestScoreKey     = "bestScore";
   this.gameStateKey     = "gameState";
 
@@ -26,7 +26,7 @@ function LocalScoreManager() {
   this.storage = supported ? window.localStorage : window.fakeStorage;
 }
 
-LocalScoreManager.prototype.localStorageSupported = function () {
+LocalStorageManager.prototype.localStorageSupported = function () {
   var testKey = "test";
   var storage = window.localStorage;
 
@@ -39,23 +39,23 @@ LocalScoreManager.prototype.localStorageSupported = function () {
   }
 };
 
-LocalScoreManager.prototype.getBestScore = function () {
+LocalStorageManager.prototype.getBestScore = function () {
   return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
-LocalScoreManager.prototype.setBestScore = function (score) {
+LocalStorageManager.prototype.setBestScore = function (score) {
   this.storage.setItem(this.bestScoreKey, score);
 };
 
-LocalScoreManager.prototype.getGameState = function () {
-    var stateJSON = this.storage.getItem(this.gameStateKey);
-    return stateJSON ? JSON.parse(stateJSON) : null;
+LocalStorageManager.prototype.getGameState = function () {
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
-LocalScoreManager.prototype.setGameState = function (gameState) {
-    this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
 };
 
-LocalScoreManager.prototype.clearGameState = function () {
-    this.storage.removeItem(this.gameStateKey);
+LocalStorageManager.prototype.clearGameState = function () {
+  this.storage.removeItem(this.gameStateKey);
 };

2015b814dfe6d76c28988d2292ebb5f29c1348d3
boazg <boaz@gezer.org.il>
2014-03-23 23:53:27 +0200

    changed value of tiles to +1.

--- js/game_manager.js
+++ js/game_manager.js
@@ -70,10 +70,12 @@ GameManager.prototype.addStartTiles = function () {
 };
 
 // Adds a tile in a random position
+var tileCounter=1;
 GameManager.prototype.addRandomTile = function () {
-  if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+  if (this.grid.cellsAvailable() && Math.random() < (this.grid.cellsAvailable()/16.0) ) {
+    var value = tileCounter;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
+    tileCounter+=1;
 
     this.grid.insertTile(tile);
   }
@@ -158,7 +160,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+          var merged = new Tile(positions.next, tile.value +1);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);

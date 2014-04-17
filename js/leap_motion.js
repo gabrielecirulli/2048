3308cf95c42f221f68da24783e91dfc717dafd5d
Sameer Goyal <sam007.goyal@gmail.com>
2014-04-17 17:54:44 +0530

    Fixing jshint errors

--- js/leap_motion.js
+++ js/leap_motion.js
@@ -3,14 +3,15 @@ var controllerOptions = {enableGestures: true};
 var cachedId;
 var sensitivity = 0.85;
 
-Leap.loop(controllerOptions, function(frame) {
+Leap.loop(controllerOptions, function (frame) {
   var gestures = frame.gestures;
-  if(gestures && gestures.length > 0) {
+  if (gestures && gestures.length > 0) {
     for (var i = 0, l = gestures.length; i < l; i++) {
       var gesture = frame.gestures[i];
-      if(gesture.type === "swipe" && gesture.id !== cachedId) {
+      if (gesture.type === "swipe" && gesture.id !== cachedId) {
         cachedId = gesture.id;
-        var event = new CustomEvent('leap-gesture', { 'detail': gesture.direction });
+        var eventDetail = { 'detail': gesture.direction };
+        var event = new CustomEvent('leap-gesture', eventDetail);
         document.dispatchEvent(event);
         break;
       }

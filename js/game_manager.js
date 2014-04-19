ff2990e4ca47d38977a827ff51d3d54164ad3629
Jan Drees <apollolv@rocketmail.com>
2014-04-19 05:05:52 +0200

    Added moves counter internally, displaying moves after game over or game won

--- js/game_manager.js
+++ js/game_manager.js
@@ -28,11 +28,7 @@ GameManager.prototype.keepPlaying = function () {
 
 // Return true if the game is lost, or has won and the user hasn't kept playing
 GameManager.prototype.isGameTerminated = function () {
-  if (this.over || (this.won && !this.keepPlaying)) {
-    return true;
-  } else {
-    return false;
-  }
+  return this.over || (this.won && !this.keepPlaying);
 };
 
 // Set up the game
@@ -44,12 +40,14 @@ GameManager.prototype.setup = function () {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
+    this.moves       = previousState.moves;
     this.over        = previousState.over;
     this.won         = previousState.won;
     this.keepPlaying = previousState.keepPlaying;
   } else {
     this.grid        = new Grid(this.size);
     this.score       = 0;
+    this.moves       = 0;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
@@ -94,6 +92,7 @@ GameManager.prototype.actuate = function () {
 
   this.actuator.actuate(this.grid, {
     score:      this.score,
+    moves:      this.moves,
     over:       this.over,
     won:        this.won,
     bestScore:  this.storageManager.getBestScore(),
@@ -107,6 +106,7 @@ GameManager.prototype.serialize = function () {
   return {
     grid:        this.grid.serialize(),
     score:       this.score,
+    moves:       this.moves,
     over:        this.over,
     won:         this.won,
     keepPlaying: this.keepPlaying
@@ -185,6 +185,9 @@ GameManager.prototype.move = function (direction) {
 
   if (moved) {
     this.addRandomTile();
+    
+    // Update the moves
+    self.moves++;
 
     if (!this.movesAvailable()) {
       this.over = true; // Game over!

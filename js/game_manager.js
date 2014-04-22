3b4e510219830a309236510ed9f76174a9e6df79
Jan Drees <apollolv@rocketmail.com>
2014-04-22 14:24:42 +0200

    Changed entire Game to 8192

--- js/game_manager.js
+++ js/game_manager.js
@@ -170,8 +170,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 8192 tile
+          if (merged.value === 8192) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }

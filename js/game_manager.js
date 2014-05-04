be1b10fdf812eb5788036c069a391d87410351bd
rhemon <rhemon@gmail.com>
2014-05-04 15:23:27 +0600

    2048

--- js/game_manager.js
+++ js/game_manager.js
@@ -28,11 +28,7 @@ GameManager.prototype.keepPlaying = function () {
 
 // Return true if the game is lost, or has won and the user hasn't kept playing
 GameManager.prototype.isGameTerminated = function () {
-  if (this.over || (this.won && !this.keepPlaying)) {
-    return true;
-  } else {
-    return false;
-  }
+  return this.over || (this.won && !this.keepPlaying);
 };
 
 // Set up the game

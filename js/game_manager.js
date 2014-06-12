65601f540b981ec425ef913f3d4f7f39de968e11
Long Vu <longvu@live.com>
2014-06-12 13:03:31 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -20,9 +20,9 @@ GameManager.prototype.restart = function () {
   this.setup();
 };
 
-// Keep playing after winning (allows going over 2048)
+// Keep playing after winning (allows going over 2049)
 GameManager.prototype.keepPlaying = function () {
-  this.keepPlaying = true;
+  this.keepPlaying = false;
   this.actuator.continueGame(); // Clear the game won/lost message
 };
 
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 2049 tile
+          if (merged.value === 2049) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }

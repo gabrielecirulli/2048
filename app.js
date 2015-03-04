e5f864bb6224db20ffef183281f5dc1ee942a038
Carlos Ferreira <carlosyells@yahoo.com>
2015-03-04 09:56:38 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/*eslint-env browser */
-var port = (process.env.VCAP_APP_PORT || 3000);
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-var http = require('http');
-
-http.createServer(function(req,res) {
-     res.writeHead(200,{'Content-Type' : 'text/plain'});
-     res.end('Hello World from Node local client!');
-}).listen(port,host);

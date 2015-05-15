e0005386620eaedf46a9e2da9f443d37c7874fbb
Christian Liebel <christian.liebel@thinktecture.com>
2015-05-15 17:22:12 +0200

    Moved gamepad logic into keyboard input manager

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -12,6 +12,32 @@ function KeyboardInputManager() {
     this.eventTouchend      = "touchend";
   }
 
+  this.pressedBefore = {};
+  this.axisValueBefore = {};
+  this.axisDirectionThreshold = 0.8;
+
+  // See standard gamepad mapping: http://www.w3.org/TR/2015/WD-gamepad-20150414/#remapping
+  this.buttons = {
+    "D_PAD_DOWN": 13,
+    "D_PAD_RIGHT": 15,
+    "D_PAD_LEFT": 14,
+    "D_PAD_UP": 12,
+    "A": 0,
+    "SELECT": 8
+  };
+
+  this.axes = {
+    "LEFT_THUMB_HORIZONTAL": 0,
+    "LEFT_THUMB_VERTICAL": 1
+  };
+
+  this.directions = {
+    "LEFT": 3,
+    "RIGHT": 1,
+    "UP": 0,
+    "DOWN": 2
+  };
+
   this.listen();
 }
 
@@ -125,15 +151,80 @@ KeyboardInputManager.prototype.listen = function () {
       self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
     }
   });
+
+  // Respond to gamepad input
+  window.addEventListener("gamepadconnected", checkGamepad);
+
+  if (navigator.getGamepads && navigator.getGamepads().length > 0) {
+    checkGamepad();
+  }
+
+  function checkGamepad() {
+    window.requestAnimationFrame(checkGamepad);
+
+    var gamepads = navigator.getGamepads();
+
+    for (var i = 0; i < gamepads.length; ++i) {
+      var pad = gamepads[i];
+
+      if (pad) {
+        if (pad.buttons[self.buttons.A].pressed) {
+          self.keepPlaying();
+        }
+
+        if (pad.buttons[self.buttons.SELECT].pressed) {
+          self.restart();
+        }
+
+        checkButton(pad, self.buttons.D_PAD_DOWN, self.directions.DOWN);
+        checkButton(pad, self.buttons.D_PAD_RIGHT, self.directions.RIGHT);
+        checkButton(pad, self.buttons.D_PAD_LEFT, self.directions.LEFT);
+        checkButton(pad, self.buttons.D_PAD_UP, self.directions.UP);
+
+        checkAxis(pad, self.axes.LEFT_THUMB_HORIZONTAL, self.directions.LEFT, self.directions.RIGHT);
+        checkAxis(pad, self.axes.LEFT_THUMB_VERTICAL, self.directions.UP, self.directions.DOWN);
+      }
+    }
+  }
+
+  function checkButton(pad, buttonId, direction) {
+    var gamepadButton = pad.buttons[buttonId];
+
+    if (!self.pressedBefore[buttonId] && gamepadButton && gamepadButton.pressed) {
+      self.emit("move", direction);
+    }
+    self.pressedBefore[buttonId] = pad.buttons[buttonId].pressed;
+  }
+
+  function checkAxis(pad, axisId, negativeDirection, positiveDirection) {
+    var axisValue = pad.axes[axisId];
+
+    if (Math.abs(self.axisValueBefore[axisId]) < self.axisDirectionThreshold && axisValue) {
+      if (axisValue > self.axisDirectionThreshold) {
+        self.emit("move", positiveDirection);
+      }
+
+      if (axisValue < self.axisDirectionThreshold * -1) {
+        self.emit("move", negativeDirection);
+      }
+    }
+    self.axisValueBefore[axisId] = axisValue;
+  }
 };
 
 KeyboardInputManager.prototype.restart = function (event) {
-  event.preventDefault();
+  if (event) {
+    event.preventDefault();
+  }
+
   this.emit("restart");
 };
 
 KeyboardInputManager.prototype.keepPlaying = function (event) {
-  event.preventDefault();
+  if (event) {
+    event.preventDefault();
+  }
+
   this.emit("keepPlaying");
 };
 

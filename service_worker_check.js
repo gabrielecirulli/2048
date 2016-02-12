f76998c011332bcca0fce4af6ba5ee46ba985e26
Shwetank Dixit <emailshwetank@gmail.com>
2016-02-12 22:08:18 +0530

    checking for service worker
    Signed-off-by: Shwetank Dixit <emailshwetank@gmail.com>

---
+++ service_worker_check.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service_worker_main.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}

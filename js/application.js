15957e1c1de2069260f693ae0ccbe29ce09c72d9
meta1203 <hancock.hunter@gmail.com>
2016-07-19 02:46:25 -0600

    Put the game manager into a variable

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var gman = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 
   // TODO: This code is in need of a refactor (along with the rest)
   var storage     = new LocalStorageManager;

c7deb11b14e15cceff6649f392496c3d9bb20798
meta1203 <hancock.hunter@gmail.com>
2016-07-19 02:53:36 -0600

    Oops, global variable

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var gman = null;
 window.requestAnimationFrame(function () {
-  var gman = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  gman = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 
   // TODO: This code is in need of a refactor (along with the rest)
   var storage     = new LocalStorageManager;

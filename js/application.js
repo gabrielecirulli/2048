9a2d39d4d92444395e61ccab0d06e477281d7baf
vyom <myadav64@gmail.com>
2016-12-17 18:35:16 +0530

    Add tizen project descriptors
    Change-Id: Ifa6e99499ef0f90adbc3acbe5461ee2b0a342ee4
    Signed-off-by: vyom <myadav64@gmail.com>

--- js/application.js
+++ js/application.js
@@ -1,4 +1,17 @@
 // Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
+
+
+window.onload = function() {
+    // Initialization job performed
+	window.requestAnimationFrame(function () {
+		  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+		});
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === "back") {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+};

71ac4cd76d64251b074294ffa3b769d814e32797
Junhao "Jerry" Zhang <house_comix@sina.com>
2018-12-27 10:14:05 -0600

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Game over!<br>Your score: "+this.score;
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;

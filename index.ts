9aa5805ec47ddd35a4a2487a8fb96d0a2bd59032
caraballofrank <frank.caraballo@leverages.jp>
2019-01-10 16:36:58 +0900

    correct index

--- index.ts
+++ index.ts
@@ -5,11 +5,7 @@ const app:express.Application = express()
 const port = 3000;
 
 app.use('/style',express.static('style'))
-<<<<<<< HEAD
-app.use('/js',express.static('dist/src'))
-=======
 app.use('/js',express.static('js'))
->>>>>>> 600042b34ca93a0fc951936adf6950a4637620c6
 app.get('/', (req:express.Request, res:express.Response) => {
   return req && res.sendFile(path.join(__dirname + '/index.html'))
 })

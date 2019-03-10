989a07b08d97ab86d4fe63a7c99467030b362d7a
John.Koepi <john.koepi@gmail.com>
2019-03-10 18:55:41 +0200

    9x9 board + show game in every board

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -1,5 +1,5 @@
 function HTMLActuator() {
-  this.tileContainer    = document.querySelector(".tile-container");
+  this.tileContainer    = document.querySelectorAll(".tile-container");
   this.scoreContainer   = document.querySelector(".score-container");
   this.bestContainer    = document.querySelector(".best-container");
   this.messageContainer = document.querySelector(".game-message");
@@ -11,7 +11,9 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
 
   window.requestAnimationFrame(function () {
-    self.clearContainer(self.tileContainer);
+    self.tileContainer.forEach(function (container) {
+      self.clearContainer(container);
+    });
 
     grid.cells.forEach(function (column) {
       column.forEach(function (cell) {
@@ -49,45 +51,47 @@ HTMLActuator.prototype.clearContainer = function (container) {
 HTMLActuator.prototype.addTile = function (tile) {
   var self = this;
 
-  var wrapper   = document.createElement("div");
-  var inner     = document.createElement("div");
-  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
-  var positionClass = this.positionClass(position);
+  this.tileContainer.forEach(function (container) {
+    var wrapper   = document.createElement("div");
+    var inner     = document.createElement("div");
+    var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+    var positionClass = self.positionClass(position);
 
-  // We can't use classlist because it somehow glitches when replacing classes
-  var classes = ["tile", "tile-" + tile.value, positionClass];
+    // We can't use classlist because it somehow glitches when replacing classes
+    var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 2048) classes.push("tile-super");
+    if (tile.value > 2048) classes.push("tile-super");
 
-  this.applyClasses(wrapper, classes);
+    self.applyClasses(wrapper, classes);
 
-  inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+    inner.classList.add("tile-inner");
+    inner.textContent = tile.value;
 
-  if (tile.previousPosition) {
-    // Make sure that the tile gets rendered in the previous position first
-    window.requestAnimationFrame(function () {
-      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
-      self.applyClasses(wrapper, classes); // Update the position
-    });
-  } else if (tile.mergedFrom) {
-    classes.push("tile-merged");
-    this.applyClasses(wrapper, classes);
+    if (tile.previousPosition) {
+      // Make sure that the tile gets rendered in the previous position first
+      window.requestAnimationFrame(function () {
+        classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+        self.applyClasses(wrapper, classes); // Update the position
+      });
+    } else if (tile.mergedFrom) {
+      classes.push("tile-merged");
+      self.applyClasses(wrapper, classes);
 
-    // Render the tiles that merged
-    tile.mergedFrom.forEach(function (merged) {
-      self.addTile(merged);
-    });
-  } else {
-    classes.push("tile-new");
-    this.applyClasses(wrapper, classes);
-  }
+      // Render the tiles that merged
+      tile.mergedFrom.forEach(function (merged) {
+        self.addTile(merged);
+      });
+    } else {
+      classes.push("tile-new");
+      self.applyClasses(wrapper, classes);
+    }
 
-  // Add the inner part of the tile to the wrapper
-  wrapper.appendChild(inner);
+    // Add the inner part of the tile to the wrapper
+    wrapper.appendChild(inner);
 
-  // Put the tile on the board
-  this.tileContainer.appendChild(wrapper);
+    // Put the tile on the board
+    container.appendChild(wrapper);
+  });
 };
 
 HTMLActuator.prototype.applyClasses = function (element, classes) {

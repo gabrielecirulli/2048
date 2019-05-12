caac9ba32b1bd4473529f47f68e60badd47747b6
Poly000 <1348292515@qq.com>
2019-05-12 23:56:19 +0800

    able to reset game

--- main.c
+++ main.c
@@ -9,16 +9,27 @@
 int main() {
 	int num[4][4]= {0},*p=num[0],i,j,num_[4][4],*q=num_[0];
 	char input;
-	srand(time(0));
-	*(p+rand()%16)=2;
-	*(p+rand()%16)=2;
-	printout(p);
+	printf("game started!\nuse R to reset\n");
 a:
+	srand(time(0));
+	i=rand()%16;
+	*(p+i)=2;
+	do
+		j=rand()%16;
+	while(j==i);
+	*(p+j)=2;
+	printout(p);
+b:
 	for(i=0; i<16; i++)
 		*(q+i)=*(p+i);
 	input=getch();
 	if(input==-32)
 		input=getch();
+	else if(input=='r'||input=='R') {
+		for(i=0; i<16; i++)
+			*(p+i)=0;
+		goto a;
+	}
 	move(p,input);
 	add(p,input);
 	move(p,input);
@@ -30,5 +41,5 @@ a:
 		printout(p);
 	} else
 		printf("Invaild type\n");
-	goto a;
+	goto b;
 }

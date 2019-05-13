ef81c81c1fca951a164e84dc93dd74a49ea2f069
Poly000 <1348292515@qq.com>
2019-05-13 12:51:46 +0800

    add R to reset

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 int main() {
 	int num[4][4]= {0},*p=num[0],i,j,num_[4][4],*q=num_[0];
 	char input;
-	printf("game started!\nuse R to reset\n");
 a:
 	srand(time(0));
 	i=rand()%16;
@@ -29,7 +28,9 @@ b:
 		for(i=0; i<16; i++)
 			*(p+i)=0;
 		goto a;
-	}
+	} else
+		if(input=='Q'||input=='q')
+			return 0;
 	move(p,input);
 	add(p,input);
 	move(p,input);
--- printout.c
+++ printout.c
@@ -1,6 +1,8 @@
 void printout(int *p) {
 	int i,j=0;
-	printf("\n-------------------------\n\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nuse R to reset\n");
+	printf("use Q to exit\n\n");
+	printf("-------------------------\n\n");
 	for(i=0; i<16; i++) {
 		if(*(p+i)!=0)
 			printf("%5d",*(p+i));

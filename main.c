7b1352a24ffff24d8fe655df69347f4e24517ca9
Poly000 <1348292515@qq.com>
2019-05-13 13:10:27 +0800

    合并源代码文件

--- main.c
+++ main.c
@@ -1,10 +1,155 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "move.c"
-#include "printout.c"
-#include "add.c"
-#include "newnum.c"
+
+void move(int *p,char input) {
+	int i,j,*k;
+	switch(input) {
+		case 'W':
+		case 'w':
+		case 72:
+W:
+			for(i=0; i<4; i++)
+				for(j=0; j<3; j++) {
+					k=p+i+4*j;
+					if(*k==0&&*(k+4)!=0) {
+						*k=*(k+4);
+						*(k+4)=0;
+						goto W;
+					}
+				}
+			break;
+		case 'S':
+		case 's':
+		case 80:
+S:
+			for(i=0; i<4; i++)
+				for(j=2; j>-1; j--) {
+					k=p+i+4*j;
+					if(*k!=0&&*(k+4)==0) {
+						*(k+4)=*k;
+						*k=0;
+						goto S;
+					}
+				}
+			break;
+		case 'a':
+		case 'A':
+		case 75:
+A:
+			for(i=0; i<4; i++)
+				for(j=0; j<3; j++) {
+					k=p+i*4+j;
+					if(*k==0&&*(k+1)!=0) {
+						*k=*(k+1);
+						*(k+1)=0;
+						goto A;
+					}
+				}
+			break;
+		case 'D':
+		case 'd':
+		case 77:
+D:
+			for(i=0; i<4; i++)
+				for(j=2; j>-1; j--) {
+					k=p+i*4+j;
+					if(*k!=0&&*(k+1)==0) {
+						*(k+1)=*k;
+						*k=0;
+						goto D;
+					}
+				}
+			break;
+	}
+}
+
+void printout(int *p) {
+	int i,j=0;
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nuse R to reset\n");
+	printf("use Q to exit\n\n");
+	printf("-------------------------\n\n");
+	for(i=0; i<16; i++) {
+		if(*(p+i)!=0)
+			printf("%5d",*(p+i));
+		else
+			printf("     ");
+		if(++j==4) {
+			printf("\n\n");
+			j=0;
+		}
+	}
+	printf("-------------------------\n\n");
+}
+
+void add(int *p,char input) {
+	int i,j,*k;
+	switch(input) {
+		case 'W':
+		case 'w':
+		case 72:
+			for(i=0; i<4; i++)
+				for(j=0; j<3; j++) {
+					k=p+i+4*j;
+					if(*k==*(k+4)) {
+						*k*=2;
+						*(k+4)=0;
+					}
+				}
+			break;
+		case 'S':
+		case 's':
+		case 80:
+			for(i=0; i<4; i++)
+				for(j=2; j>-1; j--) {
+					k=p+i+4*j;
+					if(*k==*(k+4)) {
+						*(k+4)*=2;
+						*k=0;
+					}
+				}
+			break;
+		case 'A':
+		case 'a':
+		case 75:
+			for(i=0; i<4; i++)
+				for(j=0; j<3; j++) {
+					k=p+i*4+j;
+					if(*k==*(k+1)) {
+						*k*=2;
+						*(k+1)=0;
+					}
+				}
+			break;
+		case 'D':
+		case 'd':
+		case 77:
+			for(i=0; i<4; i++)
+				for(j=2; j>-1; j--) {
+					k=p+i*4+j;
+					if(*k==*(k+1)) {
+						*(k+1)*=2;
+						*k=0;
+					}
+				}
+			break;
+defult:
+			printf("Invaild type\n");
+			break;
+	}
+}
+
+void newnum(int *p) {
+	srand(time(0));
+	int i;
+	do {
+		i=rand()%16;
+	} while(*(p+i)!=0);
+	if(rand()%3==0)
+		*(p+i)=4;
+	else
+		*(p+i)=2;
+}
 
 int main() {
 	int num[4][4]= {0},*p=num[0],i,j,num_[4][4],*q=num_[0];

62139b9e1157b7e44db1a135bfc10643c37f4a78
Poly000 <1348292515@qq.com>
2019-05-14 23:01:58 +0800

    修改版本号

--- 2048_private.h
+++ 2048_private.h
@@ -5,19 +5,19 @@
 #define 2048_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.5.0"
+#define VER_STRING	"1.0.7.4"
 #define VER_MAJOR	1
 #define VER_MINOR	0
-#define VER_RELEASE	5
-#define VER_BUILD	0
+#define VER_RELEASE	7
+#define VER_BUILD	4
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.5.0"
+#define FILE_VERSION	"1.0.7.4"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	"poly000"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.5.0"
+#define PRODUCT_VERSION	"1.0.7.4"
 
 #endif /*2048_PRIVATE_H*/

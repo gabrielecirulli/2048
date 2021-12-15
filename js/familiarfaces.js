50d37f4327f21906daef53c2c5805ca71891d630
mshiel04 <mshiel04@eecs.tufts.edu>
2021-12-14 21:23:35 -0500

    feat:testing trivial background change

--- js/familiarfaces.js
+++ js/familiarfaces.js
@@ -1,1 +1,15 @@
-console.log("Is this thing on?");
+// declare the styleSheet as a global so that a new one
+// is not created every time addColor is called
+let styleSheet;
+
+function changeBackground(className, backgroundURL) {
+    if (!stylesheet) {
+        const styleElement = document.createElement('style');
+        document.head.appendChild(styleElement);
+        styleSheet = styleElement.sheet;
+    }
+
+    // override existing background rules
+    styleSheet.insertRule("."+className+' { background: blue }', styleSheet.cssRules.length);
+}
+changeBackground("tile2");

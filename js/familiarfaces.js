6e9cdc29be2f52b85f6cd5a5767cbbb1d1394d84
mshiel04 <mshiel04@eecs.tufts.edu>
2021-12-19 22:38:18 -0500

    feat:messing around with cookies

--- js/familiarfaces.js
+++ js/familiarfaces.js
@@ -1,8 +1,7 @@
 // declare the styleSheet as a global so that a new one
 // is not created every time addColor is called
 let styleSheet;
-
-// TOOD retrieve imageData dynamically based on game ID and subsequent database GET
+document.getElementsByClassName("game-container")[0].style.display = "none";
 const imageData = 
 {
     "9284":
@@ -21,15 +20,40 @@ const imageData =
             "1nZo3NT5gs7duX-KtEWKIR5VWnKlfpi_r"
         ]
 }
-
-
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function loadGame(gameCode) {
+    imageData[gameCode].forEach((imageID, index) => {
+        num = Math.pow(2, index+1);
+        className = `.tile.tile-${num} .tile-inner`;
+        changeBackground(className, getGoogleDriveURL(imageID, 400, 400));
+    })
+    document.getElementsByClassName("game-container")[0].style.display = "block";
+}
 function getGoogleDriveURL(id, width, height) {
     return `https://drive.google.com/thumbnail?id=${id}&sz=w${width || 200}-h${height || 200}`;
 }
-
 function changeBackground(selector, url) {
     if (!styleSheet) {
-        console.log("Created stylesheet.")
         const styleElement = document.createElement('style');
         document.head.appendChild(styleElement);
         styleSheet = styleElement.sheet;
@@ -44,15 +68,19 @@ function changeBackground(selector, url) {
     );
 
 }
+
 var gameButton = document.getElementById("game-code-button");
 gameButton.onclick = () => {
     var gameCode = document.getElementById("game-code-text").value;
     if(gameCode in imageData) {
-        imageData[gameCode].forEach((imageID, index) => {
-            num = Math.pow(2, index+1);
-            className = `.tile.tile-${num} .tile-inner`;
-            changeBackground(className, getGoogleDriveURL(imageID, 400, 400));
-        })
+        setCookie("gameCode",gameCode,7);
+        loadGame(gameCode);
     }
     else alert("That's not a valid game code! Sorry!")
 }
+
+let gameCode = getCookie("gameCode");
+if (gameCode != "" && (gameCode in imageData)) {
+    loadGame(gameCode)
+}
+else alert("Looks like you don't have the right cookie on you.")

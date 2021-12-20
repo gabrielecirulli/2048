c781953b9f2bf846c821f97864e764662d3b676a
mshiel04 <mshiel04@eecs.tufts.edu>
2021-12-19 19:40:41 -0500

    feat: small text redesign, initial color scheme

--- js/familiarfaces.js
+++ js/familiarfaces.js
@@ -2,7 +2,26 @@
 // is not created every time addColor is called
 let styleSheet;
 
-function changeBackground(selector, backgroundID, width, height) {
+// TOOD retrieve imageData dynamically based on game ID and subsequent database GET
+const imageData = [
+    "1IAqNwrCzD_L-7S_BAplpNj_7YSveIYGY",
+    "1B7MiTrB8dDG_3GWxzjVGaQDRRk7UcWjm",
+    "1GoRqw3gQqP4Q6wV-7oAvhXZC7PeNt0mx",
+    "1_k3huovwuTovyGKeu_Ey_vsDT4yXkWS_",
+    "1UCxFEP2K9KVdOoanQnIkQ9SXJ2wQXY_a",
+    "1TqxAk21M0YVAqpivx3S8BpVKRpvUKwNO",
+    "1Y2gD0ov53HX04Qf1xq8F-XLssF06dEWH",
+    "1L2pWQptVbtfYHClca2Z2wTprQq3oFfDX",
+    "1VyNXfC2h5uS9Psv4gMu65pWtYUALJXKW",
+    "14MIPdrFU-FQJCvu4hrNb4ce7vJIZrqzl",
+    "1nZo3NT5gs7duX-KtEWKIR5VWnKlfpi_r"
+]
+
+function getGoogleDriveURL(id, width, height) {
+    return `https://drive.google.com/thumbnail?id=${id}&sz=w${width || 200}-h${height || 200}`;
+}
+
+function changeBackground(selector, url) {
     if (!styleSheet) {
         console.log("Created stylesheet.")
         const styleElement = document.createElement('style');
@@ -11,13 +30,16 @@ function changeBackground(selector, backgroundID, width, height) {
     }
 
     index = document.styleSheets.length - 1;
-    urlString = `https://drive.google.com/thumbnail?id=${backgroundID}&sz=w${width || 200}-h${height || 200}`
     document.styleSheets[index].insertRule(
-        `${selector}{ background: url(${urlString})
+        `${selector}{ background: url(${url})
     }`);
     document.styleSheets[index].insertRule(
         `${selector}{ background-size: cover !important}`
     );
 
 }
-changeBackground(".tile.tile-2 .tile-inner", "1IAqNwrCzD_L-7S_BAplpNj_7YSveIYGY", 400, 400);
+imageData.forEach((imageID, index) => {
+    num = Math.pow(2, index+1);
+    className = `.tile.tile-${num} .tile-inner`;
+    changeBackground(className, getGoogleDriveURL(imageID, 400, 400));
+})
--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  inner.innerHTML = `<div class="tile-text">${tile.value}</div>`
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first

e8cdb49109c52961d81919ca2acbcea78c5359c7
FOLI <you@example.com>
2022-07-25 15:40:23 +0430

    confiure service worker and its caches

--- sw.js
+++ sw.js
@@ -1,7 +1,48 @@
 // This code executes in its own worker or thread
+const urlsToCache = [
+    "index.html",
+    "style/main.css",
+    "favicon.svg",
+    "favicon.ico",
+    "/js/animframe_polyfill.js",
+    "/js/application.js",
+    "/js/bind_polyfill.js",
+    "/js/classlist_polyfill.js",
+    "/js/dom.js",
+    "/js/game_manager.js",
+    "/js/grid.js",
+    "/js/html_actuator.js",
+    "/js/in-page.js",
+    "/js/js.js",
+    "/js/keyboard_input_manager.js",
+    "/js/local_storage_manager.js",
+    "/js/tile.js",
+    "/meta/apple-touch-icon.png",
+    "/meta/apple-touch-startup-image-640x920.png",
+    "/mata/apple-touch-startup-image-640x1096.png",
+    "/style/fonts/ClearSans-Bold-webfont.woff",
+    "/style/fonts/ClearSans-Regular-webfont.woff",
+    "/style/fonts/clear-sans.css"
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service worker installed");
+    event.waitUntil(async () => {
+        const cache = await caches.open("pwa-assets");
+        return cache.addAll(urlsToCache);
+    });
 });
+
 self.addEventListener("activate", (event) => {
     console.log("Service worker activated");
 });
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request)
+      .then(cachedResponse => {
+        // It can update the cache to serve updated content on the next request
+          return cachedResponse || fetch(event.request);
+      }
+    )
+   )
+ });
